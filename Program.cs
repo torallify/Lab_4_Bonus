596c7be3252b67ebda0dd5e3ae076c8cdf3c4429
Lucero Toral <lucerotoral@quickenloans.com>
2020-02-05 14:26:24 -0500

    adding validation for special characters

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Lab_4_Bonus
 {
@@ -9,9 +10,9 @@ namespace Lab_4_Bonus
             Console.WriteLine("Welcome to the Pig Latin Translator!");
             do
             {
-                Console.Write("Please input a word or phrase: ");
-                string userInput = Console.ReadLine().ToLower();
-                Console.WriteLine(PigLatin(userInput));
+                Console.Write("\nPlease input a word or phrase: ");
+                string userInput = Console.ReadLine();
+                Console.WriteLine($"\n{PigLatin(userInput)}");
 
             } while (UserContinue());
             
@@ -34,26 +35,56 @@ namespace Lab_4_Bonus
                 return pigLatinSentence;
             }
 
-            for (int i = 0; i < word.Length; i++)
+            if (Regex.IsMatch(word, @"^[a-zA-Z']+$"))
             {
-                string c = word[i].ToString();
-                if ("aeiouAEIOU".Contains(c))
+                for (int i = 0; i < word.Length; i++)
                 {
-                    //If a word starts with a vowel, just add “way” onto the ending
-                    if (word.IndexOf(c) == 0)
+                    string c = word[i].ToString();
+
+                    if ("aeiouAEIOU".Contains(c))
                     {
-                        pigLatinString = word + "way";
-                        break;
+                        //If a word starts with a vowel, just add “way” onto the ending
+                        if (word.IndexOf(c) == 0)
+                        {
+                            pigLatinString = word + "way";
+                            break;
+                        }
+                        //If a word starts with a consonant, move all of the consonants that appear before the first vowel to the end of the word, 
+                        //then add “ay” to the end of the word.
+                        else
+                        {
+                            pigLatinString = word.Substring(word.IndexOf(c)) + word.Substring(0, word.IndexOf(c)) + "ay";
+                            break;
+                        }
                     }
-                    //If a word starts with a consonant, move all of the consonants that appear before the first vowel to the end of the word, 
-                    //then add “ay” to the end of the word.
                     else
                     {
-                        pigLatinString = word.Substring(word.IndexOf(c)) + word.Substring(0, word.IndexOf(c)) + "ay";
-                        break;
+                        pigLatinString = word;
                     }
                 }
             }
+            
+            else
+            {
+                int specialCharacterIndex;
+                string specialChaEndings = @"\|!#$%&/()=?»«@£§€{}.-;<>_,";
+                foreach (var item in specialChaEndings)
+                {
+                    if (word.Contains(item) && (word.IndexOf(item) == (word.Length - 1)))
+                    {
+                        specialCharacterIndex = word.IndexOf(item);
+                        pigLatinString = word.Substring(0, specialCharacterIndex);
+
+                        pigLatinString = PigLatin(pigLatinString) + word.Substring(specialCharacterIndex);
+                    }
+                    
+                    else if (word.Contains(item))
+                    {
+                        pigLatinString = word;
+                    }
+                        
+                } 
+            }          
             return pigLatinString;
         }
 
